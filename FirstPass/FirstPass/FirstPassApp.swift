147f1ca9ea81471a59da1e0f0f3a51e098fdbe0e
Math'BookPro <mathieubazin@hotmail.fr>
2026-04-14 17:41:08 +0200

    create project

---
+++ FirstPass/FirstPass/FirstPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstPassApp.swift
+//  FirstPass
+//
+//  Created by Mathieu Bazin on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FirstPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
